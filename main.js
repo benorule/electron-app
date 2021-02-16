c678b6d077872bea3333aad6a709e91b28154659
benorule <benorule@outlook.com>
2021-02-16 12:53:30 +1030

    write index.js script that will read markup files

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  win.loadFile('index.html')
-}
-
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('ready', function(){
+    let window = new BrowserWindow({width:800, height:600})
+    window.loadURL(path.join('file://', __dirname, 'static/index.html'))
 })
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+app.on('close', function() {
+    window = null
 })
